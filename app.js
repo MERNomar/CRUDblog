9e1c967f881685aaddb9832a05f17048316d9517
Omar Ahmed <109013442+MERNomar@users.noreply.github.com>
2023-09-03 22:14:59 +0300

    files

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+let app = express()
+
+app.set('view engine' , 'ejs')
+
+
+app.listen(8080 , () => {
+    console.log("listening in http://localhost:8080")
+})
+
+app.get('/' , (req , res) => {
+    res.render('index')
+})
+
+app.get('/about' , (req , res) => {
+    res.sendFile('./about.html' , {root : __dirname})
+})
+
+app.get('/404' , (req , res) => {
+    res.sendFile('./404.html' , {root : __dirname})
+})
+
+
+
+app.use( (req , res) => {
+    res.status(404).sendFile('./404.html' , {root : __dirname})
+})sda

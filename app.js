1b906beb03694bde5a5e3c56e54e84b17bbd9ca2
Omar Ahmed <109013442+MERNomar@users.noreply.github.com>
2023-09-06 02:43:20 +0300

    adding get request to the database and render it

--- app.js
+++ app.js
@@ -1,30 +1,35 @@
 const express = require('express')
-let app = express()
+const app = express()
+const mongoose = require('mongoose')
+const Blog = require('./models/blog')
 
+
+const url = "mongodb+srv://omar:omar123@cluster0.pvkokfm.mongodb.net/?retryWrites=true&w=majority"
 app.set('view engine' , 'ejs')
 app.use(express.static(__dirname + '/public'));
 
-
-let blogs = [
-    {blog : "how to create express app" , snippet : "search google you dork"},
-    {blog : "how to create mongodb database" , snippet : "just search google !!"},
-    {blog : "now to create react app" , snippet : "just write npm create react app dumpass !"}
-]
-
-app.listen(3000 , () => {
+mongoose.connect(url).then(() => {app.listen(3000 , () => {
     console.log("listening in http://localhost:3000")
-})
+})})
+.catch(err => {console.log(err)})
+
+
+app.get('/all-blogs' , (req , res) => {
+  })
+
 
 app.get('/' , (req , res) => {
-    res.render('index' , {blogs})
+    Blog.find().then(blogs => {
+        res.render('index' , {blogs ,title : " | Home"})
+    })
 })
 
 app.get('/about' , (req , res) => {
-    res.render('about')
+    res.render('about',{title : " | About"})
 })
 
 app.get('/newblog' , (req , res) => {
-    res.render('newblog')
+    res.render('newblog' , {title : " | new Blog"})
 })
 
 

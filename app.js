739720930219b9e19315400649da9dc33b62d86e
Omar Ahmed <109013442+MERNomar@users.noreply.github.com>
2023-09-08 01:14:04 +0300

    adding details and delete

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const Blog = require('./models/blog')
+app.use(express.urlencoded({extended : true}))
 
 
 const url = "mongodb+srv://omar:omar123@cluster0.pvkokfm.mongodb.net/?retryWrites=true&w=majority"
@@ -13,12 +14,18 @@ mongoose.connect(url).then(() => {app.listen(3000 , () => {
 })})
 .catch(err => {console.log(err)})
 
+app.get('/' , (req , res) =>{
+    res.redirect('/blogs')
+})
 
-app.get('/all-blogs' , (req , res) => {
-  })
+app.get('/blogs/thing' , (req , res) => {
+    Blog.find().then(blogs => {
+        res.render('index' , {blogs ,title : " | Home"})
+    })
+})
 
 
-app.get('/' , (req , res) => {
+app.get('/blogs' , (req , res) => {
     Blog.find().then(blogs => {
         res.render('index' , {blogs ,title : " | Home"})
     })
@@ -31,6 +38,39 @@ app.get('/about' , (req , res) => {
 app.get('/newblog' , (req , res) => {
     res.render('newblog' , {title : " | new Blog"})
 })
+
+app.get('/blogs/:id' , (req , res) => {
+    const id = req.params.id;
+    Blog.findById(id)
+    .then(result => {
+        res.render('details' , {blog : result , title : " | details" })
+    })
+    
+   
+})
+
+
+
+app.post('/blogs' , (req , res) =>{
+    const blog = new Blog(req.body)
+    blog.save()
+    .then((e) => {res.redirect('/')})
+    .catch(err => {console.log(err)})
+})
+
+app.delete('/blogs/:id' , (req , res) => {
+    const id = req.params.id;
+    Blog.findByIdAndDelete(id)
+    .then((result) =>{
+        res.json({redirect : '/blogs' })
+    } )
+   
+
+})
+
+app.use(function(req, res){
+    res.render('404' , {title : " | 404"});
+});
 
 
 
